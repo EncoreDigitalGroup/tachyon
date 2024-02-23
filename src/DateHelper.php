d2dd2cafdcf3e7bcde58a875e8e7171fa8d47a84
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-02-23 15:46:16 -0600

    Convert to Tachyon and Backwards Support DateHelper

--- src/DateHelper.php
+++ src/DateHelper.php
@@ -8,79 +8,30 @@ class DateHelper
 {
     protected string $source_timezone;
     protected string $target_timezone;
+    protected Tachyon $tachyon;
 
     public function __construct($target_timezone = 'UTC', $source_timezone = 'UTC')
     {
-        $this->source_timezone = $source_timezone;
-        $this->target_timezone = $target_timezone;
+        $this->tachyon = new Tachyon($source_timezone, $target_timezone);
     }
 
     public function happeningSoon($start_time = null): bool
     {
-        if($start_time == null) {
-            return false;
-        }
-        
-        $TargetTimezone = $this->target_timezone;
-        $StartTime = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone);
-        $OneHourFromStartTime = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone)->subHours(1);
-        $Now = Carbon::now()->setTimezone($TargetTimezone);
-        $IsToday = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone)->isToday();
-        if (($Now < $StartTime) && ($Now > $OneHourFromStartTime) && ($IsToday)) {
-            return true;
-        }
-
-        return false;
+        return $this->tachyon->startingSoon($start_time);
     }
 
     public function happeningNow($start_time = null, $end_time = null): bool
     {
-        if($start_time == null || $end_time == null) {
-            return false;
-        }
-        
-        $TargetTimezone = $this->target_timezone;
-        $StartTime = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone);
-        $EndTime = Carbon::createFromFormat('Y-m-d H:i:s', $end_time)->setTimezone($TargetTimezone);
-        $IsToday = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone)->isToday();
-        if ($StartTime < Carbon::now()->setTimezone($TargetTimezone) && $EndTime > Carbon::now()->setTimezone($TargetTimezone) && $IsToday) {
-            return true;
-        }
-
-        return false;
+        return $this->tachyon->happeningNow($start_time, $end_time);
     }
 
     public function withinThreeHours($start_time = null): bool
     {
-        if($start_time == null) {
-            return false;
-        }
-        
-        $TargetTimezone = $this->target_timezone;
-        $Now = Carbon::now()->setTimezone($TargetTimezone);
-        $StartTime = Carbon::createFromFormat('Y-m-d H:i:s', $start_time);
-        $ThreeHoursBefore = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone)->subHours(3);
-        if ($Now > $ThreeHoursBefore && $Now < $StartTime) {
-            return true;
-        }
-
-        return false;
+        return $this->tachyon->withinThreeHours($start_time);
     }
 
     public function isToday($start_time = null): bool
     {
-        if($start_time == null) {
-            return false;
-        }
-        
-        $TargetTimezone = $this->target_timezone;
-        $StartTime = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone);
-        $IsToday = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone)->isToday();
-        $Now = Carbon::now()->setTimezone($TargetTimezone);
-        if ($IsToday && $Now < $StartTime) {
-            return true;
-        }
-
-        return false;
+        return $this->tachyon->isToday($start_time);
     }
 }
---
+++ src/Tachyon.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace EncoreDigitalGroup\Tachyon;
+
+use Carbon\Carbon;
+
+class Tachyon
+{
+    protected string $source_timezone;
+    protected string $target_timezone;
+
+    public function __construct($source_timezone = 'UTC', $target_timezone = 'UTC')
+    {
+        $this->source_timezone = $source_timezone;
+        $this->target_timezone = $target_timezone;
+    }
+
+    public function startingSoon($start_time = null): bool
+    {
+        if($start_time == null) {
+            return false;
+        }
+
+        $TargetTimezone = $this->target_timezone;
+        $StartTime = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone);
+        $OneHourFromStartTime = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone)->subHours(1);
+        $Now = Carbon::now()->setTimezone($TargetTimezone);
+        $IsToday = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone)->isToday();
+        if (($Now < $StartTime) && ($Now > $OneHourFromStartTime) && ($IsToday)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function happeningNow($start_time = null, $end_time = null): bool
+    {
+        if($start_time == null || $end_time == null) {
+            return false;
+        }
+
+        $TargetTimezone = $this->target_timezone;
+        $StartTime = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone);
+        $EndTime = Carbon::createFromFormat('Y-m-d H:i:s', $end_time)->setTimezone($TargetTimezone);
+        $IsToday = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone)->isToday();
+        if ($StartTime < Carbon::now()->setTimezone($TargetTimezone) && $EndTime > Carbon::now()->setTimezone($TargetTimezone) && $IsToday) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function withinThreeHours($start_time = null): bool
+    {
+        if($start_time == null) {
+            return false;
+        }
+
+        $TargetTimezone = $this->target_timezone;
+        $Now = Carbon::now()->setTimezone($TargetTimezone);
+        $StartTime = Carbon::createFromFormat('Y-m-d H:i:s', $start_time);
+        $ThreeHoursBefore = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone)->subHours(3);
+        if ($Now > $ThreeHoursBefore && $Now < $StartTime) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function isToday($start_time = null): bool
+    {
+        if($start_time == null) {
+            return false;
+        }
+
+        $TargetTimezone = $this->target_timezone;
+        $StartTime = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone);
+        $IsToday = Carbon::createFromFormat('Y-m-d H:i:s', $start_time)->setTimezone($TargetTimezone)->isToday();
+        $Now = Carbon::now()->setTimezone($TargetTimezone);
+        if ($IsToday && $Now < $StartTime) {
+            return true;
+        }
+
+        return false;
+    }
+}

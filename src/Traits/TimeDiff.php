8c5e60a6cfc7d4bd5462829f8a4ae509831ae76c
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-03-26 09:38:57 -0500

    Add Exceptions to Traits

--- src/Traits/TimeDiff.php
+++ src/Traits/TimeDiff.php
@@ -4,13 +4,30 @@ namespace EncoreDigitalGroup\Tachyon\Traits;
 
 use Carbon\Carbon;
 use DateTime;
+use EncoreDigitalGroup\Tachyon\Exceptions\TachyonException;
 
 trait TimeDiff
 {
+    /**
+     * @throws TachyonException
+     */
     public function unixDiffInSeconds(Carbon|DateTime $targetDateTime): float|int
     {
+        $targetDateTimeString = $targetDateTime->format('Y-m-d H:i:s');
+
         $sourceDateTime = Carbon::createFromFormat('Y-m-d H:i:s', Carbon::now(), $this->targetTimezone);
-        $targetDateTime = Carbon::createFromFormat('Y-m-d H:i:s', $targetDateTime, $this->targetTimezone);
+
+        if (!$sourceDateTime) {
+            throw new TachyonException();
+        }
+
+        $sourceDateTime->toDateTimeString();
+
+        $targetDateTime = Carbon::createFromFormat('Y-m-d H:i:s', $targetDateTimeString, $this->targetTimezone);
+
+        if (!$targetDateTime) {
+            throw new TachyonException('Invalid targetDateTime Provided.');
+        }
 
         return $targetDateTime->diffInSeconds($sourceDateTime);
     }

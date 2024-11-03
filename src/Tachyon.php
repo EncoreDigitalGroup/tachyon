7bf6b8faae16831795aeb20e4f4073b47c2c6bc6
Marc Beinder <marc@marcbeinder.com>
2024-11-03 08:30:38 -0600

    [Traits] Add GenericHelpers

--- src/Tachyon.php
+++ src/Tachyon.php
@@ -2,11 +2,13 @@
 
 namespace EncoreDigitalGroup\Tachyon;
 
+use EncoreDigitalGroup\Tachyon\Traits\GenericHelpers;
 use EncoreDigitalGroup\Tachyon\Traits\KeyTimeIndicators;
 use EncoreDigitalGroup\Tachyon\Traits\TimeDiff;
 
 class Tachyon
 {
+    use GenericHelpers;
     use KeyTimeIndicators;
     use TimeDiff;
 

1219539028c4388a2c5ab9bdb0446be9d6a33f02
onairmarc <onairmarc@users.noreply.github.com>
2024-11-03 14:49:13 +0000

    Dusting

--- src/Tachyon.php
+++ src/Tachyon.php
@@ -17,27 +17,28 @@ class Tachyon
     protected string $sourceTimezone;
     protected string $targetTimezone;
 
+    public function __construct(string $targetTimezone = 'UTC', string $sourceTimezone = 'UTC')
+    {
+        $this->sourceTimezone = $sourceTimezone;
+        $this->targetTimezone = $targetTimezone;
+    }
+
     /** @experimental */
     public static function make(?string $targetTimezone = null, ?string $sourceTimezone = null): self
     {
         $defaultTimezone = 'UTC';
 
-        if (!isset(static::$instance) && is_null($targetTimezone) && is_null($sourceTimezone)) {
+        if (! isset(static::$instance) && is_null($targetTimezone) && is_null($sourceTimezone)) {
             static::$instance = new self($defaultTimezone, $defaultTimezone);
+
             return static::$instance;
         } elseif (isset(static::$instance) && is_null($targetTimezone) && is_null($sourceTimezone)) {
             return static::$instance;
-        } elseif (isset(static::$instance) && (!is_null($targetTimezone) || !is_null($sourceTimezone))) {
+        } elseif (isset(static::$instance) && (! is_null($targetTimezone) || ! is_null($sourceTimezone))) {
             static::$instance = new self($targetTimezone ?? $defaultTimezone, $sourceTimezone ?? $defaultTimezone);
         }
 
         return static::$instance;
-    }
-
-    public function __construct(string $targetTimezone = 'UTC', string $sourceTimezone = 'UTC')
-    {
-        $this->sourceTimezone = $sourceTimezone;
-        $this->targetTimezone = $targetTimezone;
     }
 
     public function setSource(string $sourceTimezone = 'UTC'): void
